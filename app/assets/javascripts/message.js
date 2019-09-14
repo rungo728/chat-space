be7a6b3475c0fd2c2f3a9c69a8d408f299b4b6c2
rungo728 <rufii72865@gmail.com>
2019-09-14 13:49:55 +0900

    update３message.js
    返ってきたJSONをdoneメソッドで受取り、HTMLを作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,46 @@
 $(function(){
+  function buildHTML(message) {
+    if ( message.image.url ) {
+      var html = 
+       `<div class="message">
+         <div class="upper-message">
+           <p class="upper-message__username">
+             ${message.user.name}
+           </p>
+           <p class="upper-message__date">
+             ${message.created_at.strftime("%Y/%m/%d %H:%M")}
+           </p>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+             ${message.content}
+            </p>
+             <img src=${message.image.url}>
+           </div>
+         </div>`
+      return html;
+    } else {
+      var html =
+      `<div class="message">
+        <div class="upper-message">
+          <p class="upper-message__username">
+            ${message.user.name}
+          </p>
+          <p class="upper-message__date">
+            ${message.created_at.strftime("%Y/%m/%d %H:%M")}
+          </p>
+         </div>
+        <div class="lower-message">
+           <p class="lower-message__content">
+              ${message.content}
+           </p>
+         </div>
+       </div>`
+      return html; 
+    };
+  } 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log('OK');
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
@@ -12,5 +51,8 @@ $(function(){
       processData: false,
       contentType: false
     })  
+    .done(function(data){
+      var html =  buildHTML(data);
+    })
   })
 }); 
