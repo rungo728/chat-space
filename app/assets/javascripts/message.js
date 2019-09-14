42aa5ecbe45876d3c13ea55a8e448e98d093fc3b
rungo728 <rufii72865@gmail.com>
2019-09-14 16:05:22 +0900

    update5 message.js
    9. HTMLを追加した分、メッセージ画面を下にスクロールする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,6 +54,7 @@ $(function(){
     .done(function(data){
       var html =  buildHTML(data);
       $('messages').append(html);
+      $('messages').animate({scrollTop: $('messages')[0].scrollHeight}, 'fast');
       $('form')[0].reset();
     })
   })
