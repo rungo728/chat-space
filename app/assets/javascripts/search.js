05adb90612a3ec22ed2404c590e921d3ed9bc419
rungo728 <rufii72865@gmail.com>
2019-09-16 21:11:55 +0900

    Asynchronous communication when an event occurs
    キーが入力されるというイベントの度に非同期通信できるようにする

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,24 @@
+
 $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input);
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })  
+
+    .done(function(user) {
+      $("#user-search-result").empty();
+      if (user.length !== 0) {
+        user.forEach(function(user){
+          appendUserToSearchList(user);
+        });
+      }
+      else {
+        appendNoUserToSearchList("一致するユーザーはありません");
+      } 
+    })
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def index
+    @users =  User.where('name LIKE(?)',"%{params[:keyword]}%").where.not(id:current_user.id)
+    respond_to do |format|
+      format.html { redirect_to new_group_path  }
+      format.json
+    end  
+  end
+
   def edit
   end
 
@@ -15,4 +23,4 @@ class UsersController < ApplicationController
   def user_params
       params.require(:user).permit(:name, :email)
   end
-end
+end    
