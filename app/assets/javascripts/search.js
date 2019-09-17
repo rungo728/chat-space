19c5e1d4e63d2eec7b875a086daf4c911282acc5
rungo728 <rufii72865@gmail.com>
2019-09-17 23:23:22 +0900

    Update2 search.js
    追加ボタンをクリックされたユーザーの名前を、チャットメンバーの部分に追加し、検索結果からは消す
    削除を押すと、チャットメンバーから削除される

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -16,6 +16,19 @@ $(function() {
      search_list.append(html);
   }
   
+  var members = $(".chat-group-users")
+  
+  function addUser(id, name) {
+    var html = 
+      `<div class="chat-group-user clearfix " id="chat-group-user-${id}">
+        <input name='group[user_ids][]' type=hidden value=${id}>
+          <p class="chat-group-user__name">${name}</p>
+          <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
+       </div>`
+    members.append(html);
+  }
+
+
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
@@ -40,5 +53,15 @@ $(function() {
     .fail(function() {
       alert('ユーザー検索に失敗しました')
     })  
+  });
+  $(document).on("click", ".user-search-add", function() {
+    var userId = $(this).data("user-id")
+    var userName = $(this).data("user-name")
+    $(this).parent().remove()
+    addUser(userId, userName)
+  
+  });
+  $(document).on("click", ".user-search-remove", function() {
+    $(this).parent().remove();
   });
 });
