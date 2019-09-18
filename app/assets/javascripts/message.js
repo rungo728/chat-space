0178eac046642b99e9a68f67c995c4beeb1c36e7
rungo728 <rufii72865@gmail.com>
2019-09-18 20:42:43 +0900

    Update message.js haml(first commit)
    表示されているメッセージのHTMLにid情報を追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
     var content = message.content ? `${ message.content }` : "";
     var image = message.image ? `<img src= ${ message.image }>` : "";
     var html = 
-      `<div class="message">
+      `<div class="message" data-message-id="${message.id}">
         <div class="upper-message">
           <p class="upper-message__username">
             ${message.user_name}
